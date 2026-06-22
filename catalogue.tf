1184a02beff493c9905839a664f12f849cf800f9
megha <megha@gmail.com>
2026-06-23 02:21:46 +0530

    22-6

--- catalogue.tf
+++ catalogue.tf
@@ -1,9 +1,9 @@
-resource "aws_instance" "catalogue" {
-    ami = data.aws_ami.ami_id.id
-    instance_type = var.environment == "dev" ? "t3.micro" : "t3.medium"
-    subnet_id = split(",", data.aws_subnet.private.id)[0]
-    vpc_security_group_ids = data.aws_ssm_parameter.catalogue_sg_id.value
-    tags = {
-        Name = "${local.common_name}_catalogue"
-    }
-}
+# resource "aws_instance" "catalogue" {
+#     ami = data.aws_ami.ami_id.id
+#     instance_type = var.environment == "dev" ? "t3.micro" : "t3.medium"
+#     subnet_id = split(",", data.aws_subnet.private.id)[0]
+#     vpc_security_group_ids = data.aws_ssm_parameter.catalogue_sg_id.value
+#     tags = {
+#         Name = "${local.common_name}_catalogue"
+#     }
+# }
