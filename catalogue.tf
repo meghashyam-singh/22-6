4a87f0ceb5184cf84ce967321f39eec529e46a36
megha <megha@gmail.com>
2026-06-23 02:30:11 +0530

    22-6

--- catalogue.tf
+++ catalogue.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "catalogue" {
     ami = data.aws_ami.ami_id.id
     instance_type = var.environment == "dev" ? "t3.micro" : "t3.medium"
-    subnet_id = split(",", data.aws_subnet.private_subnet_id.value)[0]
+    subnet_id = split(",", data.aws_ssm_parameter.private_subnet_id.value)[0]
     vpc_security_group_ids = data.aws_ssm_parameter.catalogue_sg_id.value
     tags = {
         Name = "${local.common_name}_catalogue"
